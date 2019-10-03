fef9be58eecf54ef2a844dfd00ac8c16e1d02a4d
Patty C <patrickcorcoran10@gmail.com>
2019-10-03 13:34:38 -0500

    commit with text added to products page

--- client/src/pages/Products/Products.js
+++ client/src/pages/Products/Products.js
@@ -3,8 +3,86 @@ import React, { Component } from 'react'
 export default class Products extends Component {
     render() {
         return (
-            <div>
-                <p>Products Page</p>
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-md-1'>
+                    </div>
+                    <div className='col-md-10'>
+                        <h5>OUR PRODUCTS</h5>
+                        <h6>ONLY THE HIGHEST QUALITY AND CARE STANDARDS</h6>
+                    </div>
+                    <div className='col-md-1'>
+                    </div>
+                </div>
+                <div className='row'>
+                <div className='col-md-1'>
+                    </div>
+                    <div className='col-md-5'>
+                        <ul>
+                            <li>To improve the patient care experience</li>
+                            <li>To provide a safe quality product</li>
+                            <li>To use a natural essential oil with known benefits as a non-drug option</li>
+                            <li>To give patients an option to use less medication and improve quality of their experience while in the health care system</li>
+                            <li>To engage nurses and caregivers with an easy to use application</li>
+                        </ul>
+                    </div>
+                    <div className='col-md-5'>
+                        <p>product image</p>
+                    </div>
+                    <div className='col-md-1'>
+                    </div>
+                </div>
+                <br/>
+                <div className='row'>
+                    <div className='col-md-1'>
+
+                    </div>
+                    <div className='col-md-10'>
+                        <h5>FEEDBACK ABOUT LAVENDER NITES:</h5>
+                        <ul>
+                            <li>Nurses reported patients seemed more relaxed and liked having an option other than medication</li>
+                            <li>Patients thanked caregivers for their lavender massage</li>
+                            <li>Nurses stated applying Lavender nites put them in touch with their patients</li>
+                            <li>Non-patients like the product too! People who have tried the cream benefited from its relaxing effect</li>
+                        </ul>
+                        <br/>
+                        <h5>ORDERING INFORMATION</h5>
+                        <h6><strong>Wholesale Order Requirements</strong></h6>
+                        <ul>
+                            <li>Minimum order is 150 units or 1 case</li>
+                            <li>Each case contains two sealed bags each with 75 units</li>
+                            <li>Cost per case is $147.00 or .98 per unit</li>
+                            <li>Shipping and handling additional. Purchase 3 cases and shipping is free</li>
+                            <li>Provide us with your Federal Tax I.D. number or sales resale license number</li>
+                            <li>Orders are typically shipped within 24 to 48 hours of being placed</li>
+                            <li>Terms: Master Card, Visa, Discover accepted. Company check, net 30 day terms</li>
+                            <li>New customers are required to pre-pay</li>
+                            <li>No orders shipped to overdue accounts</li>
+                            <li>Returned checks are subject to a $30.00 service fee</li>
+                            <li>All sales are considered final</li>
+                        </ul>
+                        <br/>
+                        <h6><strong>Freight and Shipping</strong></h6>
+                        <p>Orders are shipped UPS (ground service). Any other shipping requests will result in additional costs to the customer</p>
+                        <p>Customers refusing an order will be charged both the outbound freight and the cost of UPS charges to return the refused order to Flora Pharmica LLC®</p>
+                        <br/>
+                        <h6><strong>Damaged Goods</strong></h6>
+                        <p>We will give full credit for damaged or defective product if you contact us within one week from receipt. Maintain the damaged product, shipping box and packing materials for UPS to inspect upon pick up. Call our main number for further direction</p>
+                        <br/>
+                        <h5>CONTACT FLORA PHARMICA LLC®</h5>
+                        <p><strong>Phone Orders:</strong> Orders may be placed at 414-403-1854, 8:00 am to 5pm Central Time Monday through Friday.</p>
+                        <p><strong>Email:</strong> customerservice@florapharmica.com</p>
+                        <p><strong>Mail Check as Payment to: </strong></p>
+                        <p>Flora Pharmica LLC ®</p>
+                        <p>6650 W. State St. Suite 138</p>
+                        <p>Wauwatosa, WI, 53213</p>
+                        <br/>
+                        <button>Experience Our Product</button>
+                    </div>
+                    <div className='col-md-1'>
+                        
+                    </div>
+                </div>
             </div>
         )
     }
